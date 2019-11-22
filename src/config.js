49d734aba058104b833d2d6956a3c89a8631ebc8
Afif <afif.patel@cgi.com>
2019-11-22 08:38:27 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "fifaws-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://al2rutp5a4.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_OjawR2wg8",
+      APP_CLIENT_ID: "1s9bdr0033j56dhtsdb8btaugg",
+      IDENTITY_POOL_ID: "us-west-2:e0ec9750-f564-48ab-8ac7-42e6d5bd68af"
+    }
+};
